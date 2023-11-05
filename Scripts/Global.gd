56bc9285ece5504302a15003b52812a7f1943208
CosmicBubble300 <dlalondedavidson@gmail.com>
2023-11-04 20:25:13 -0400

    still broken...

--- Scripts/Global.gd
+++ Scripts/Global.gd
@@ -5,7 +5,6 @@ extends Node
 ### Variables ###
 
 ## Oxygen Management ##
-var tank_spare = 0         # Amount of oxygen tanks the character is carrying 
 var tank_full = 100  
 
 var tank_list = []
